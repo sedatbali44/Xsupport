cf626511ca64353680446d535f0cd11ada380f7c
Sedat Bali <sedatbali44@gmail.com>
2025-06-20 16:13:19 +0300

    Initial commit

---
+++ src/main/java/com/Xsupport/XsupportApplication.java
@@ -0,0 +1,13 @@
+package com.Xsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Xsupport/XsupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Xsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
